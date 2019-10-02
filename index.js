689e78892ba9b04a8b991f4bfa03d93a991265d9
oluwaleye victor <oluwasquarelsv@gmail.com>
2019-10-02 17:35:43 +0100

    fibonnaci solution

--- index.js
+++ index.js
@@ -78,3 +78,15 @@ var factorial = function(n) {
   return n * factorial(n - 1);
 };
 console.log("factorial", factorial(5));
+/*
+Write a function that outputs the nth Fibonnaci number. A number in this sequence is found by adding up the two numbers before it.
+Fibonnaci's sequence:
+input    0 1 2 3 4 5 6  7  8  9 ...
+output   0 1 1 2 3 5 8 13 21 34 ...
+What is the time complexity? Can you think of optimizing your solution? (Hint: look up dynamic programming)
+*/
+var fibonnaci = function(n) {
+  if (n === 1 || n === 0) return n;
+  return fibonnaci(n - 1) + fibonnaci(n - 2);
+};
+console.log(fibonnaci(8));
